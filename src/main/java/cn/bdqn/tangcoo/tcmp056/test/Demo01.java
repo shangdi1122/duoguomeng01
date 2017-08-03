b1f9fce3149fc0ab672eeb45892be560e10d1042
DGMVip <1139008935@qq.com>
2017-08-03 18:46:09 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcoo/tcmp056/test/Demo01.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcoo.tcmp056.test;
+
+import org.junit.Test;
+
+/**
+ * Created by lenovo on 2017/8/3.
+ */
+public class Demo01 {
+    @Test
+    public void Demo01() {
+        System.out.println("hello word");
+    }
+}
